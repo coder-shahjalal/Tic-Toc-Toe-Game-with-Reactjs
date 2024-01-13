60ec55e7964abec56d83c5aa649967e9b749476d
md shahjalal <shahjalal.freelancer@gmail.com>
2024-01-13 17:42:27 +0600

    hover player next turn

--- src/components/TicTacToe.jsx
+++ src/components/TicTacToe.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react';
+import { useState } from "react";
 import Board from "./Board";
 
-const PLAYER_X ="X"
-const PLAYER_O ="O"
-
+const PLAYER_X = "X";
+const PLAYER_O = "O";
 
 const TicTacToe = () => {
-  const [tiles,setTiles] = useState(Array(9).fill(null))
-  const [playerTurn,setPlayerTurn] = useState(PLAYER_X)
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [playerTurn, setPlayerTurn] = useState(PLAYER_X);
 
-  const handleTileClick = (index) =>{
-
-    if (tiles[index] !==null){
-      return
+  const handleTileClick = (index) => {
+    if (tiles[index] !== null) {
+      return;
     }
 
-    const newTiles = [...tiles]
+    const newTiles = [...tiles];
     console.log(newTiles);
-    newTiles[index] = playerTurn
-    setTiles(newTiles)
-    playerTurn === PLAYER_X ? setPlayerTurn(PLAYER_O) : setPlayerTurn(PLAYER_X)
-
-  }
-
+    newTiles[index] = playerTurn;
+    setTiles(newTiles);
+    playerTurn === PLAYER_X ? setPlayerTurn(PLAYER_O) : setPlayerTurn(PLAYER_X);
+  };
 
   return (
     <div>
       <h1> Tic Tac Toe </h1>
-      <Board tiles={tiles} onTileClick={handleTileClick}/>
+      <Board
+        playerTurn={playerTurn}
+        tiles={tiles}
+        onTileClick={handleTileClick}
+      />
     </div>
   );
 };
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,5 +1,15 @@
-function Tile({className, value,onClick }) {
-  return <div onClick={onClick} className={`tile ${className}`}>{value}</div>;
+//https://www.youtube.com/watch?v=4Gt_YyGf6B0&t=1352s
+
+function Tile({ className, value, onClick, playerTurn }) {
+  let hoverClass = null;
+  if (value == null && playerTurn != null) {
+    hoverClass = `${playerTurn.toLowerCase()}-hover`;
+  }
+  return (
+    <div onClick={onClick} className={`tile ${className} ${hoverClass}`}>
+      {value}
+    </div>
+  );
 }
 
 export default Tile;
